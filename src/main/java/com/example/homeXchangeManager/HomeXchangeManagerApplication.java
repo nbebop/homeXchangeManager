7dcb35f3888c94cf2c7d737c9673342771820164
nbebop <nizar.bikti@gmail.com>
2023-04-03 15:20:27 +0200

    init gradle

---
+++ src/main/java/com/example/homeXchangeManager/HomeXchangeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeXchangeManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeXchangeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeXchangeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homeXchangeManager/HomeXchangeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeXchangeManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeXchangeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
