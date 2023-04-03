c9d60d20dd243e2612907fd7308ebb28ec5854ac
nbebop <nizar.bikti@gmail.com>
2023-04-03 16:43:01 +0200

    set up gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,35 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.0.5"
-	id("io.spring.dependency-management") version "1.1.0"
+    java
+    id("org.springframework.boot") version "3.0.5"
+    id("io.spring.dependency-management") version "1.1.0"
+    id("org.graalvm.buildtools.native") version "0.9.20"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- src/main/java/com/example/homeXchangeManager/HomeXchangeManagerApplication.java
+++ src/main/java/com/example/homeXchangeManager/HomeXchangeManagerApplication.java
@@ -1,4 +1,4 @@
-package com.example.homeXchangeManager;
+package com.example.homexchangemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HomeXchangeManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomeXchangeManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HomeXchangeManagerApplication.class, args);
+    }
 
 }
