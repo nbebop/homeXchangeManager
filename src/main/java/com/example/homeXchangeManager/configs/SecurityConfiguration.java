0b7a4034a5fe83e51ba4907de887c016d5969abb
nbebop <nizar.bikti@gmail.com>
2023-04-17 14:55:20 +0200

    Add security rule for admin access
    Add user query

--- src/main/java/com/example/homeXchangeManager/configs/SecurityConfiguration.java
+++ src/main/java/com/example/homeXchangeManager/configs/SecurityConfiguration.java
@@ -25,12 +25,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        http.authorizeRequests().antMatchers("/registration/**", "/js/**", "/css/**", "/img/**").permitAll().anyRequest()
-                .authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
-                .invalidateHttpSession(true).clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout")
-                .permitAll();
+    // more specific rules come first
+        http.authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/registration/**", "/js/**", "/css/**", "/img/**")
+                .permitAll().anyRequest().authenticated().and()
+                .formLogin().loginPage("/login").permitAll().and()
+                .logout().invalidateHttpSession(true).clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout").permitAll();
 
     }
 }
--- src/main/java/com/example/homeXchangeManager/repositories/UserRepository.java
+++ src/main/java/com/example/homeXchangeManager/repositories/UserRepository.java
@@ -2,10 +2,14 @@ package com.example.homeXchangeManager.repositories;
 
 import com.example.homeXchangeManager.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
+
+    User findByEmailPassword(String email, String password);
+
 }
