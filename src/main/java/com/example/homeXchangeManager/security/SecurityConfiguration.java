69591c028242061b41c3b32e3238b35ac427f0df
nbebop <nizar.bikti@gmail.com>
2023-05-07 12:33:52 +0200

    Clean up security config

--- src/main/java/com/example/homeXchangeManager/security/SecurityConfiguration.java
+++ src/main/java/com/example/homeXchangeManager/security/SecurityConfiguration.java
@@ -7,11 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -36,36 +32,6 @@ public class SecurityConfiguration {
                 .httpBasic();
 
         return http.build();
-    }
-
-    /**
-    @Bean
-    public UserDetailsService user() {
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password("admin")
-                .roles("ADMIN")
-                .build();
-        UserDetails user = User.builder()
-                .username("user")
-                .password("user")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
-    }*/
-
-    protected void configure(HttpSecurity http) throws Exception {
-        // more specific rules come first
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/registration/**", "/js/**", "/css/**", "/img/**")
-                .permitAll().anyRequest().authenticated().and()
-                .formLogin().loginPage("/login").permitAll().and()
-                .logout().invalidateHttpSession(true).clearAuthentication(true)
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/login?logout").permitAll();
     }
 
     private CustomUserDetailsService userDetailsService;
