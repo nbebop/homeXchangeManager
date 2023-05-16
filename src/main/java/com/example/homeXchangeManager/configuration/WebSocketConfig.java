0f10aa1d0840428de8baefd331713dd659433744
MarioArocaPaez <73229438+MarioArocaPaez@users.noreply.github.com>
2023-05-16 22:02:55 +0200

    Add WebSocket configuration and message controller for chat feature

---
+++ src/main/java/com/example/homeXchangeManager/configuration/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.example.homeXchangeManager.configuration;
+
+import ch.qos.logback.classic.pattern.MessageConverter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
+    }
+}
