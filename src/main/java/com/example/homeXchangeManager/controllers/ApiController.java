a2fb2b0e09129bf1733bceeaa1d467455420ae46
nbebop <nizar.bikti@gmail.com>
2023-05-16 14:27:17 +0200

    Integrate login
    * separate api controllers
    * small fixes

--- src/main/java/com/example/homeXchangeManager/controllers/ApiController.java
+++ src/main/java/com/example/homeXchangeManager/controllers/ApiController.java
@@ -1,41 +1,45 @@
 package com.example.homeXchangeManager.controllers;
 
+import com.example.homeXchangeManager.dto.LoginDto;
 import com.example.homeXchangeManager.dto.RegisterDto;
 import com.example.homeXchangeManager.models.Role;
 import com.example.homeXchangeManager.models.User;
 import com.example.homeXchangeManager.repositories.RoleRepository;
 import com.example.homeXchangeManager.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Collections;
-
 @Controller
-public class AuthController {
+public class ApiController {
     private AuthenticationManager authenticationManager;
     private UserRepository userRepository;
     private RoleRepository roleRepository;
     private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+    public ApiController(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    @ModelAttribute("user")
-    public RegisterDto registerDto() {
-        return new RegisterDto();
-    }
+    @PostMapping("/api/auth/register")
+    public ResponseEntity<String> registerApi(@RequestBody RegisterDto registerDto) {
+        if (userRepository.existsByUsername(registerDto.getUsername())) {
+            return new ResponseEntity<>("Username is taken!", HttpStatus.BAD_REQUEST);
+        }
 
-    @PostMapping("register")
-    public String register(@ModelAttribute("user") RegisterDto registerDto) {
         User user = new User();
         user.setUsername(registerDto.getUsername());
         user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
@@ -50,11 +54,18 @@ public class AuthController {
         user.setCity(registerDto.getCity());
         user.setPostalCode(registerDto.getPostalCode());
         user.setCountry(registerDto.getCountry());
-        // anyone who registers is a user
         user.setRoles(Collections.singletonList(new Role("USER")));
 
         userRepository.save(user);
-        return "login";
+
+        return new ResponseEntity<>("User registration successful!", HttpStatus.OK);
     }
 
+    @PostMapping("/api/auth/login")
+    public ResponseEntity<String> loginApi(@RequestBody LoginDto loginDto) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return new ResponseEntity<>("User signed success", HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/homeXchangeManager/controllers/HomeController.java
+++ src/main/java/com/example/homeXchangeManager/controllers/HomeController.java
@@ -2,12 +2,25 @@ package com.example.homeXchangeManager.controllers;
 
 import com.example.homeXchangeManager.dto.RegisterDto;
 import com.example.homeXchangeManager.models.User;
+import com.example.homeXchangeManager.repositories.RoleRepository;
+import com.example.homeXchangeManager.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    public HomeController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
     @GetMapping("/login")
     public String login() {
         return "login";
