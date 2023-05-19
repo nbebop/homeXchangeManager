f3aafa015915fc25deae09de6d81caecda15d6ad
nbebop <nizar.bikti@gmail.com>
2023-05-19 10:30:48 +0200

    Redirect on login based on user role

--- src/main/java/com/example/homeXchangeManager/controllers/HomeController.java
+++ src/main/java/com/example/homeXchangeManager/controllers/HomeController.java
@@ -4,6 +4,7 @@ import com.example.homeXchangeManager.repositories.RoleRepository;
 import com.example.homeXchangeManager.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,18 +15,28 @@ public class HomeController {
     private UserRepository userRepository;
     private RoleRepository roleRepository;
     private PasswordEncoder passwordEncoder;
+
     @Autowired
     public HomeController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-    @GetMapping("/login")
+
+    @GetMapping({"/login", "/"})
     public String login() {
         return "login";
     }
 
-    @GetMapping("/")
-    public String home() {
-        return "login";
+    @GetMapping("/success")
+    public String successLogin(Authentication authResult) {
+        String role = authResult.getAuthorities().toString();
+
+        if (role.contains("ADMIN")) {
+            return "admin_page";
+        } else if (role.contains("USER")) {
+            return "home_page";
+        }
+        //fall back
+        return "error/404";
     }
 
     @GetMapping("/listing")
