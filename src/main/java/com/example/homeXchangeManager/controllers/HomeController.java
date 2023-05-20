582159c20862917ec06b3f77d1976b91b0116a7d
nbebop <nizar.bikti@gmail.com>
2023-05-21 00:55:07 +0200

    Pass listing image to frontend
    * create get request

--- src/main/java/com/example/homeXchangeManager/controllers/HomeController.java
+++ src/main/java/com/example/homeXchangeManager/controllers/HomeController.java
@@ -4,11 +4,16 @@ import com.example.homeXchangeManager.models.Listing;
 import com.example.homeXchangeManager.service.ListingService;
 import com.example.homeXchangeManager.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -61,6 +66,21 @@ public class HomeController {
         model.addAttribute("listings", getAllListing());
 
         return "home_page";
+    }
+
+    /**
+     * Controller to display listing images
+     * and to display the image in frontend:
+     * <img th:src="@{'/display/image/' + ${clothing.id}}">
+     */
+    @GetMapping("/listing/image/{id}")
+    public ResponseEntity<Byte[]> displayItemImage(@PathVariable long id) {
+        Listing listing = listingService.findByListingId(id);
+        Byte[] image = listing.getImage();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+
+        return new ResponseEntity<>(image, headers, HttpStatus.OK);
     }
 
     private List<Listing> getAllListing() {
