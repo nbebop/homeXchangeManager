b8ae3eabc772508c4c4bbd4891abb33b12e448de
nbebop <nizar.bikti@gmail.com>
2023-05-28 17:48:26 +0200

    Create image entity

---
+++ src/main/java/com/example/homeXchangeManager/models/Image.java
@@ -0,0 +1,57 @@
+package com.example.homeXchangeManager.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "images")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(name = "content_type")
+    private String contentType;
+
+    @Lob
+    private byte[] data;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+}
