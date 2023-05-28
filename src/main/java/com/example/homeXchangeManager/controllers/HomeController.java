702a3fa39c6b3f1bf4e34f1c8b121e5183ebe68c
nbebop <nizar.bikti@gmail.com>
2023-05-29 00:44:52 +0200

    Fix passed attributes to account

--- src/main/java/com/example/homeXchangeManager/controllers/HomeController.java
+++ src/main/java/com/example/homeXchangeManager/controllers/HomeController.java
@@ -105,24 +105,15 @@ public class HomeController {
     private List<Listing> getAllListing() {
         return listingService.findAll();
     }
-    private List<User> getAllUsers() {
-        return userService.findAll();
-
-    }
-
-//    @GetMapping("/account")
-//    public String account() {
-//        return "account";
-//    }
 
     @GetMapping("/account")
     public String account(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userService.findByUsername(auth.getName());
+        List<Listing> listings = listingService.findByOwner(user);
 
-        model.addAttribute("currentUser", user);
-        //model.addAttribute("listings", getAllListing());
-        //model.addAttribute("users", getAllUsers());
+        model.addAttribute("user", user);
+        model.addAttribute("listings", listings);
 
         return "account";
     }
