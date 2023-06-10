46fbd11ed88c34846aa7788409003260494f2236
nbebop <nizar.bikti@gmail.com>
2023-06-11 01:35:15 +0200

    Implement listing rating

---
+++ src/main/java/com/example/homeXchangeManager/service/ListingRatingService.java
@@ -0,0 +1,10 @@
+package com.example.homeXchangeManager.service;
+
+import com.example.homeXchangeManager.dto.ListingDto;
+import com.example.homeXchangeManager.dto.RatingDto;
+import com.example.homeXchangeManager.models.Listing;
+
+public interface ListingRatingService {
+    void save(RatingDto ratingDto);
+    double calculateAverageRating(Listing listing);
+}
