273a3878a50e020e16efed2dbc2aff91a543dab1
Magdalena Pakuła <magdalena.pakula001@gmail.com>
2023-06-11 02:28:15 +0200

    - added successful message notification after adding listing/registation

--- src/main/java/com/example/homeXchangeManager/controllers/ListingController.java
+++ src/main/java/com/example/homeXchangeManager/controllers/ListingController.java
@@ -16,6 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -61,7 +62,7 @@ public class ListingController {
      * Code to display images in frontend
      */
     @PostMapping("/new_listing")
-    public String createListing(@Valid @ModelAttribute("listing") ListingDto listingDto, @RequestParam("images") MultipartFile[] images, BindingResult bindingResult) {
+    public String createListing(@Valid @ModelAttribute("listing") ListingDto listingDto, @RequestParam("images") MultipartFile[] images, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         if (bindingResult.hasErrors()) {
             String errorMessages = "";
             for (ObjectError error : bindingResult.getAllErrors()) {
@@ -77,6 +78,7 @@ public class ListingController {
         User owner = userService.findByUsername(auth.getName());
         listingDto.setOwner(owner);
         listingService.save(listingDto, images);
+        redirectAttributes.addFlashAttribute("successMessage", "Listing created successfully!");
 
         return "redirect:/listing";
     }
